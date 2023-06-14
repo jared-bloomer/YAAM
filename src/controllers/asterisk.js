275c420e99ef978b46388b830be28e7ca46bd18e
Jared-Bloomer <jared.bloomer@coxautoinc.com>
2023-06-13 20:06:07 -0400

    updating comment

--- src/controllers/asterisk.js
+++ src/controllers/asterisk.js
@@ -6,9 +6,8 @@ const username = config.get('asterisk.username');
 const password = config.get('asterisk.password');
 const events = config.get('asterisk.events');
 
-/* See https://www.npmjs.com/package/asterisk-manager for info on how to use the asterisk-manager */
+/* See hhttps://github.com/pipobscure/NodeJS-AsteriskManager/tree/master for info on how to use the asterisk-manager */
 
 var ami = new require('asterisk-manager')(port,host,username,password,events);
 ami.keepConnected();
-
 
